a82d06e5a08784689808195d8d5f11538acffc15
xiexing <1835901429@qq.com>
2020-03-22 10:41:54 +0800

    日志输出

--- blog_client/lib/utils/mysql_utils.js
+++ blog_client/lib/utils/mysql_utils.js
@@ -7,19 +7,20 @@ const config = require("../../config");
  * @returns {Promise<p>}
  */
 exports.sqlQuery = function (sql, arr) {
-    return new Promise(function (resolve, reject) {
-        try {
-            let connection = mysql.createConnection(config.mysql);
-            connection.query(sql, arr, function (err, data) {
-                if (err)
-                    reject(err);
-                connection.release();
-                resolve(data);
-            });
-        } catch (err) {
-            resolve(err);
-        }
-    });
+  return new Promise(function (resolve, reject) {
+    try {
+      let connection = mysql.createConnection(config.mysql);
+      connection.query(sql, arr, function (err, data) {
+        if (err)
+          reject(err);
+        connection.release();
+        resolve(data);
+      });
+    } catch (err) {
+      console.log(err);
+      resolve(err);
+    }
+  });
 };
 
 /**
@@ -28,27 +29,28 @@ exports.sqlQuery = function (sql, arr) {
  * @param arr
  */
 exports.sqlPoolQuery = async function (sql, arr) {
-    return new Promise(function (resolve, reject) {
-        try {
-            let connnection = mysql.createPool(config.mysql);
-            connnection.getConnection(function (err, conn) {
-                if (err) {
-                    reject(err);
-                }
-                try {
-                    conn.query(sql, arr, function (err, result) {
-                        if (err) {
-                            reject(err);
-                        }
-                        conn.release();
-                        resolve(result);
-                    })
-                } catch (e) {
-                    reject(e);
-                }
-            })
-        } catch (err) {
-            reject(err);
+  return new Promise(function (resolve, reject) {
+    try {
+      let connnection = mysql.createPool(config.mysql);
+      connnection.getConnection(function (err, conn) {
+        if (err) {
+          reject(err);
         }
-    });
+        try {
+          conn.query(sql, arr, function (err, result) {
+            if (err) {
+              reject(err);
+            }
+            conn.release();
+            resolve(result);
+          })
+        } catch (e) {
+          reject(e);
+        }
+      })
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+  });
 };

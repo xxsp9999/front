cac359b40baae6f71775d798f87d7f5be68537f6
xiexing <1835901429@qq.com>
2020-03-22 16:46:21 +0800

    添加日志输出

--- blog_client/lib/model/login_log.js
+++ blog_client/lib/model/login_log.js
@@ -2,11 +2,13 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let LoginLogSchema = new Schema({
-    login_user: {
-        id: Number,
-        user_name: String,
-    },
-    login_time: Date,
+  login_user: {
+    user_id: Number,
+    user_name: String,
+    status: Boolean,
+    msg: String
+  },
+  login_time: Date,
 });
 let Login_log = mongoose.model('Login_log', LoginLogSchema);
 module.exports = Login_log;
